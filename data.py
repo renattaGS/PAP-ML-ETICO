c22a319a4c7b61859236e0ebd3e51ee695de5a08
renattaGS <renattags20@gmail.com>
2021-04-14 13:48:14 -0500

    First changes

--- data.py
+++ data.py
@@ -1,12 +1,23 @@
-
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- project: Ethical Machine Learning applied to  data from Techo.org                                   -- #
 # -- script: data.py : python script for data collection                                                 -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- author: renattaGS                                                                                   -- #
 # -- license: GPL-3.0 License                                                                            -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- repository: https://github.com/renattaGS/PAP-ML-ETICO                                               -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
-dict_test = {'key_a': 'a', 'key_b': 'b'}
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+def lectura_datos(file_path: str, sheet: str, column_dateformat: str) -> pd.DataFrame:
+    data = pd.read_excel(file_path, sheet)
+    data[column_dateformat] = pd.to_datetime(data[column_dateformat], format='%Y').dt.year
+    return data
+
+
+def reporte_profiling(df: 'Data Frame', output_name: str):
+    report = ProfileReport(df, minimal=True)
+    report.to_file(output_file=output_name)
